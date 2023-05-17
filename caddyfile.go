4f45f66bf5d7ec22f17fb0695cd94f132fabbe35
otaconix <stefanhetzwaantje@gmail.com>
2023-05-17 20:23:22 +0200

    domains: merge zones with the same name (#45)
    * domains: merge zones with the same name
    
    * append to nil slice!
    
    Co-authored-by: Matt Holt <mholt@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Matt Holt <mholt@users.noreply.github.com>

--- caddyfile.go
+++ caddyfile.go
@@ -66,7 +66,7 @@ func parseApp(d *caddyfile.Dispenser, _ interface{}) (interface{}, error) {
 				if app.Domains == nil {
 					app.Domains = make(map[string][]string)
 				}
-				app.Domains[zone] = names
+				app.Domains[zone] = append(app.Domains[zone], names...)
 			}
 
 		case "update_only":
